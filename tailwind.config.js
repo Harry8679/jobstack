44daaaf380249063fe79072ad9d8f888c64f3d5c
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-04 20:31:32 +0200

    Tail Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        plusjakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
