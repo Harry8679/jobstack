cf8d35d32f2af653d8be5434c2ffe442c2836ee8
Harry EMARH <emarh.harry.code@gmail.com>
2025-05-05 21:47:00 +0200

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/connexion" element={<Login />} />
         <Route path="/inscription" element={<Signup />} />
